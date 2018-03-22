f759128df721d35fa25b127f370e868eab11bba8
Jan Fidler <honzaf28@gmail.com>
2018-03-22 23:42:38 +0100

    hiding building, new materials and textures, sm_rowbuilding_01_destroyed new mesh

--- Assets/Scripts/Triggers/HideOnTrigger.cs
+++ Assets/Scripts/Triggers/HideOnTrigger.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class HideOnTrigger : MonoBehaviour
 {
     public GameObject child;
+    public float hidingSpeed = 10f;
+    public float hidingHeight = -1f;
+
+    Vector3 targetHideScale;
+    Vector3 initScale;
+
+    private void Awake()
+    {
+        child.SetActive(true);
+
+        initScale = child.transform.localScale;
+
+        targetHideScale = child.transform.localScale;
+        targetHideScale.y = hidingHeight;
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,9 +28,8 @@ public class HideOnTrigger : MonoBehaviour
 
         if (other.CompareTag("Player"))
         {
-            Debug.Log("2");
-
-            child.SetActive(false);
+            // child.SetActive(false);
+            StartCoroutine("Hide", hidingSpeed);
         }
     }
     private void OnTriggerExit(Collider other)
@@ -23,8 +38,37 @@ public class HideOnTrigger : MonoBehaviour
 
         if (other.CompareTag("Player"))
         {
-            Debug.Log("2");
-            child.SetActive(true);
+            //child.SetActive(true);
+            StartCoroutine("Unhide", hidingSpeed);
+        }
+    }
+
+
+    private IEnumerator Hide(float time)
+    {
+        StopCoroutine("Unhide");
+        float elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+
+            child.transform.localScale = Vector3.Lerp(child.transform.localScale, targetHideScale, hidingSpeed * Time.deltaTime);
+
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator Unhide(float time)
+    {
+        StopCoroutine("Hide");
+
+        float elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            child.transform.localScale = Vector3.Lerp(child.transform.localScale, initScale, hidingSpeed * Time.deltaTime);
+
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
         }
     }
 }
