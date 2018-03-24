c607351c9b3715a1253d87ba21ccb00e324640b6
Jan Fidler <honzaf28@gmail.com>
2018-03-24 23:43:42 +0100

    fixování chyb prefabů, nová vrstva entities, npc fix, hýbání mraků

---
+++ Assets/Scripts/Offsetters/CloudOffseter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudOffseter : MonoBehaviour
+{
+    public float updateTime = 50f;
+    float nextUpdate;
+
+    public float cloudMovementSpeed = 0.0009f;
+    public float cloudScalingSpeed = 0.01f;
+
+    public float cloudSizeRandomMultiplier = 2f;
+    Light myLight;
+    Vector3 initPos;
+    float initCloudSize;
+
+    float newCloudSize;
+    Vector3 newPos;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        myLight = GetComponent<Light>();
+        initPos = transform.position;
+        initCloudSize = myLight.cookieSize;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       if(Time.time > nextUpdate)
+        {
+            nextUpdate = Time.time + updateTime;
+
+            newPos = initPos;
+            newPos.x += Random.Range(-1000, 1000);
+            newPos.z += Random.Range(-1000, 1000);
+
+            newCloudSize = initCloudSize + Random.Range(-initCloudSize * cloudSizeRandomMultiplier, initCloudSize * cloudSizeRandomMultiplier);
+        }
+
+        //lerp pos
+        transform.position = Vector3.Lerp(transform.position, newPos, cloudMovementSpeed * Time.deltaTime);
+        //lerp size
+        myLight.cookieSize = Mathf.Lerp(initCloudSize, newCloudSize, cloudScalingSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Offsetters/WaterOffseter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterOffseter : MonoBehaviour
+{
+    public float waterSpeed;
+
+    Material myMaterial;
+	// Use this for initialization
+	void Start ()
+    {
+        myMaterial = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float newOffset = Time.time * waterSpeed;
+        Vector2 newVector = new Vector2(newOffset, 0);
+
+        myMaterial.SetTextureOffset("_MainTex", newVector);
+        myMaterial.SetTextureOffset("_DetailAlbedoMap", -newVector);
+
+    }
+}
