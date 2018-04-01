cb62bb3a5c4b989fa9f3e876e8e68589ef2b42ee
Jan Fidler <honzaf28@gmail.com>
2018-04-01 21:40:31 +0200

    Group Selected Objects custom plugin (ctrl + g)

---
+++ Assets/Standard Assets/Editor/pagpCustomEditorFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class pagpCustomEditorFunctions : MonoBehaviour
+{
+    [MenuItem("PAGP_Custom/ GroupSelectedObjects %g")]
+
+
+    // create group of objects
+    static void GroupObject()
+    {
+        Vector3 groupOrigin = FindSelectionCenter();
+
+        GameObject groupEmpty = new GameObject("GROUP (size: " + Selection.transforms.Length + ")" + "(" + Selection.transforms[0].name + ")");
+        groupEmpty.transform.position = groupOrigin;
+
+        foreach(Transform gameObj in Selection.transforms)
+        {
+            gameObj.SetParent(groupEmpty.transform, true);
+        }
+    }
+
+    static Vector3 FindSelectionCenter()
+    {
+        Vector3 center = new Vector3(0, 0, 0);
+        float count = 0f;
+
+        foreach (Transform obj in Selection.transforms)
+        {
+            center += obj.position;
+            count++;
+        }
+
+        return center / count;
+    }
+}
