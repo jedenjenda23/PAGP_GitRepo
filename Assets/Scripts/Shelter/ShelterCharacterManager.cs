af63c295ab69d8e29c1ff815da7f7c0397582502
Jan Fidler <honzaf28@gmail.com>
2018-04-09 10:08:15 +0200

    loading characters to shelter scene

---
+++ Assets/Scripts/Shelter/ShelterCharacterManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelterCharacterManager : MonoBehaviour
+{
+    public static ShelterCharacterManager instance;
+    public Transform[] spawnPoints;
+
+    private void Awake()
+    {
+        if (ShelterCharacterManager.instance) Destroy(gameObject);
+        else ShelterCharacterManager.instance = this;
+
+        SpawnCharactersInShelter();
+    }
+
+    public void SpawnCharactersInShelter()
+    {
+        if (GlobalGameManager.instance)
+        {
+            if (GlobalGameManager.instance.shelterCharacters.Length > 0 && spawnPoints.Length >= GlobalGameManager.instance.shelterCharacters.Length)
+            {
+                for (int i = 0; i < spawnPoints.Length; i++)
+                {
+                    GameObject charToSpawn = GlobalGameManager.instance.shelterCharacters[i];
+
+                    GameObject newCharacter = Instantiate(charToSpawn, spawnPoints[i], false);
+                    newCharacter.transform.localPosition = Vector3.zero;
+
+                    //deactivate unnecessary sh*ts
+                    newCharacter.GetComponent<GC_PlayableHumanoidCharacter>().enabled = false;
+                    newCharacter.GetComponent<Inventory>().enabled = false;
+                }
+            }
+        }
+    }
+}
