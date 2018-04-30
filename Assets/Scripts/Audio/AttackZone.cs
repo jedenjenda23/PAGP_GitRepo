ac71522888ac96ee5fb3ec31489f755f265888cc
Inirdin <inirdin.eventis@gmail.com>
2018-04-30 15:07:00 +0200

    Particly | Almost functional music system

---
+++ Assets/Scripts/Audio/AttackZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackZone : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioManager.instance.Attack();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        AudioManager.instance.StopAttack(50);
+    }
+}
